b62e09bebcd15646401ddd144aed247535b56885
Torkel Bjørnson-Langen <torkel.bjornson@gmail.com>
2014-03-25 14:00:51 +0100

    work

--- invoice/admin.py
+++ invoice/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Client, Invoice, InvoiceLine
+from .models import Account, Client, Invoice, InvoiceLine
 #from .forms import InvoiceInlineForm
+
+from django.conf.urls import patterns, url
+from django.http import HttpResponse
+
+
+
+# TODO
+# list all invoices on client?
 
 
 class InvoiceInline (admin.TabularInline):
@@ -16,6 +24,24 @@ class InvoiceAdmin (admin.ModelAdmin):
 
     inlines = [InvoiceInline]
 #    exclude = 'lines',
+    #list_display = 'client__display_name', # not possible?
+
+    def get_urls (self):
+        return patterns ('',
+            (r'^pdf_view/(?P<invoice_id>\d+)/$', self.admin_site.admin_view (self.pdf_view)),
+        ) + super (InvoiceAdmin, self).get_urls()
+
+    # custom views
+    def pdf_view (self, request, invoice_id):
+        print request.path
+        return HttpResponse ('Fuck you!')
+
+    # custom actions
+    actions = ('make_pdf',)
+    def make_pdf (self, request, queryset):
+        print queryset
+        self.message_user (request, 'PDF ready for download')
+    make_pdf.short_description = 'Create PDF of selected invoice(s)'
 
     # @todo text&comment => extra fieldset
     #readonly_fields = 'date',
@@ -24,11 +50,54 @@ class InvoiceAdmin (admin.ModelAdmin):
     #raw_id_fields = 'lines',
 
 
+
 class ClientAdmin (admin.ModelAdmin):
-    list_display = 'name', 'contact', 'email'
-#    fields = ('name', 'address', ('contact', 'email'), 'comment')
+    ''' Payer '''
+    list_display = '__unicode__', 'contact', 'phone', 'email'
+    #list_display = 'name', 'contact', 'phone', 'email'
+    #fields = ('name', 'address', ('contact', 'email'), 'comment')
+
+#    URLS = patterns ('',
+#        (r'^my_view1/$', self.admin_site.admin_view (self.my_view1)),
+#        (r'^my_view2/$', self.admin_site.admin_view (self.my_view2, cacheable=True)),
+#    )
+
+    # not in use
+    def get_urls (self):
+        return patterns ('',
+            # @todo view that takes id to row?
+            #(r'^my_view/$', self.my_view),
+            (r'^my_view/$', self.admin_site.admin_view (self.my_view)),
+            #(r'^my_view/$', self.admin_site.admin_view (self.my_view, cacheable=True)),
+        ) + super (ClientAdmin, self).get_urls()
+
+    # @todo howto use admin templates?
+    def my_view (self, request):
+        print request.path
+        return HttpResponse ('Fuck you!')
 
 
+
+admin.site.register (Account)
 admin.site.register (Client, ClientAdmin)
 admin.site.register (Invoice, InvoiceAdmin)
-#admin.site.register (InvoiceLine)
+
+
+
+'''
+@todo move to __init__.py
+#from django.contrib.admin.sites import AdminSite
+#class MyAdminSite (AdminSite):
+class MyAdminSite (admin.sites.AdminSite):
+    'Add some own views/urls to the default admin site'
+
+#    def get_urls (self):
+#        return patterns ('',
+#            (r'^my_admin_view/$', self.my_view),
+#        ) + super (MyAdminSite, self).get_urls()
+
+    def my_view (self, *args):
+        print args
+
+admin.site = MyAdminSite()
+'''
--- invoice/models.py
+++ invoice/models.py
@@ -1,18 +1,30 @@
-""" Register invoices and render to PDF.
+# encoding: utf8
+"""
+Register invoices and render to PDF.
+
+TODO:
+* AddressField, PhoneField?
+* get_absolute_url() - Any object that has a URL that uniquely identifies it should define this method.
+* unique on org_no
+* webpage for client (1:n?)
+* list of contact persons
 
 """
 
 from django.db import models
 import datetime
 
+#from django.core.exceptions import ValidationError
+#raise ValidationError('Lack of empaty')
 
-class Client (models.Model):
+
+class Account (models.Model):
     name = models.CharField (max_length=100)
     address = models.TextField()
-    contact = models.CharField (max_length=64, blank=True, verbose_name='Contact person')
+    org_no = models.CharField (max_length=32, blank=True)
+    account_no = models.CharField (max_length=32)
+    phone = models.CharField (max_length=32, blank=True)
     email = models.EmailField (blank=True)
-    # Q: email to client or to contact person
-    # a: checkbox to select?
     comment = models.TextField (blank=True)
 
     class Meta:
@@ -21,26 +33,77 @@ class Client (models.Model):
     def __unicode__ (self):
         return self.name
 
+    def clean (self):
+        self.address = self.address.replace ('\r', '')
+
+    '''
+    def save (self, *args, **kwargs):
+        #print dir(self)
+        self.address = self.address.replace ('\r', '')
+        super (Account,self).save (*args, **kwargs)
+    '''
+
+
+class Client (models.Model):
+    display_name = models.CharField (max_length=32, blank=True, help_text='Our name')
+    name = models.CharField (max_length=100, help_text='Company name')
+    # @todo howto display help text after (not bellow) field? css?
+    #display_name = models.CharField (max_length=32, blank=True, help_text='Our name')
+    #name = models.CharField (max_length=100, help_text='Company name')
+    address = models.TextField()
+    org_no = models.CharField (max_length=32, blank=True)
+    phone = models.CharField (max_length=32, blank=True)
+    email = models.EmailField (blank=True)
+    #contact = models.CharField (max_length=64, blank=True, verbose_name='Contact person')
+    # @todo owner?
+    contact = models.CharField ('contact person', max_length=64, blank=True)
+    comment = models.TextField (blank=True)
+
+    class Meta:
+        ordering = 'name',
+
+    def __unicode__ (self):
+        return self.display_name or self.name
+
+
 
 class Invoice (models.Model):
     date = models.DateField (auto_now=True)
+    due = models.DateField (default=datetime.date.today() + datetime.timedelta(days=20))
+    # @todo default must be callable!
     client = models.ForeignKey (Client)
-    # changed to ForeignKey on InvoiceLine instead
-    #lines = models.ManyToManyField ('InvoiceLine')
+    #account = models.ForeignKey (Account)
+    account = models.ForeignKey (Account, null=True)   # tmp tmp
+    contact = models.CharField ('contact person', max_length=64, blank=True)
+    #contact = models.CharField (max_length=64, blank=True, verbose_name='Deres ref.:')
+    #our_contact = models.CharField (max_length=64, blank=True, verbose_name='Vår ref.:')
     text = models.TextField (blank=True, help_text='Extra text to put on the invoice')
     comment = models.TextField (blank=True, help_text='Internal comments')
-    #invoice_no = models.IntegerField()
-    # due_date / betalingsfrist / betingelser (14 dager)
-    #due_date = models.DateField(default=datetime.date.today() + datetime.timedelta(days=14))
+    #invoice_no = models.IntegerField() # @todo auto-sequence
+    # @todo auto-generate invoice_no. no db field.
 
     def __unicode__ (self):
-        return self.client.name + ' : ' + self.date.isoformat()
+        return self.date.isoformat() + ': ' + unicode(self.client) + \
+               ' : ' + self.invoiceline_set.first().text    # slow
+        #return self.client.name + ' : ' + self.date.isoformat()
+
+    def invoice_no (self):
+        return '1234'
+
+#    def _get_full_name (self):
+#        "Returns the persons full name"
+#        return '%s %s' % (self.first_name, self.last_name)
+#    full_name = property (_get_full_name)
+
 
 
 class InvoiceLine (models.Model):
     invoice = models.ForeignKey (Invoice)
     text = models.CharField (max_length=100)
+    #quantity = models.TextField (max_length=32)
     quantity = models.FloatField (default=1)  # @todo DecimalField or RationalField
+    #unit = models.CharField (max_length=16)
     amount = models.DecimalField (max_digits=12, decimal_places=2)
+    # @todo amount -> unit_price?
     def __unicode__ (self):
         return self.text
